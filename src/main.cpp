06b60ba2b2e7b0282ea2f125fdce6ea7c9b58611
sdj <sdj@pyrite-n3.cs.iastate.edu>
2019-04-18 14:00:57 -0500

    First attempt at parallelization

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,15 @@ double clamp(double val, double min, double max);
 vec3 rgbToVec(int r, int g, int b);
 
 int main(){
+    MPI_Init(NULL, NULL);
+    int i;
+    double my_num;
+    int my_rank, comm_sz;
+    MPI_Comm comm;
+    comm = MPI_COMM_WORLD;
+    MPI_Comm_size(comm, &comm_sz);
+    MPI_Comm_rank(comm, &my_rank);
+
     Canvas canvas(512, 512);
     int samples = 16;
     int num_threads = 4;
@@ -27,9 +36,8 @@ int main(){
     double v_width = 2;
     double v_height = 2;
     double v_depth = 4;
-
     double wall_r = 100000; //radius for wall spheres
-   
+
     Scene scene;
     scene.addObject(new Sphere(vec3(-.5, -2,10), 1, rgbToVec(231, 76, 60))); //red
     scene.addObject(new Sphere(vec3(.5, -1, 9.5), 1, rgbToVec(200, 200, 200))); //white
@@ -43,58 +51,81 @@ int main(){
     scene.addObject(new Sphere(vec3(0, 0, wall_r + 20), wall_r, rgbToVec(200, 200, 200))); //far wall
 
 
-    //array of vectors to hold raw colors
-    vec3* pixels = new vec3[canvas.width * canvas.height];
 
-    auto start = std::chrono::high_resolution_clock::now();
+    if(my_rank == 0){
 
+      double* pixel_row = (double *) malloc(3 * sizeof(double) * canvas.width);
+      int row_start; 
+      MPI_Status status; 
+      //array of vectors to hold raw colors
+      vec3* pixels = new vec3[canvas.width * canvas.height];
+    
+      auto start = std::chrono::high_resolution_clock::now();
+    
+      
 
-    #pragma omp parallel for num_threads(num_threads) 
-    for (int y = 0; y < canvas.height; y++){
-        //each thread needs seed for call to rand_r() because rand() is locking
-        unsigned int seed = (unsigned int)omp_get_thread_num();
-        for (int x = 0; x < canvas.width; x++){
-            vec3 color(0,0,0);
-        
-            for (int i = 0; i < samples; i++){
+      for(int k = 0; k < canvas.height; k++){
+	
+        MPI_Recv(pixel_row, 3 * canvas.width, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &status);
+	row_start = status.MPI_TAG;
+        for(int l = row_start * canvas.width; l < canvas.width; l++){
+          pixels[l] = new vec3(*pixel_row(l * 3), *pixel_row(l * 3 + 1), *pixel_row(l * 3 + 2));
+	}
+      }
 
-                //compute location on projection plane
-                double u = v_width * (double(x) + rand_real(&seed))/double(canvas.width) - (v_width/2);
-                double v = v_height * (double(y) + rand_real(&seed))/double(canvas.height) - (v_height/2);
+      #pragma omp parallel for num_threads(num_threads)
+      for (int y = 0; y < canvas.height; y++){
+          for (int x = 0; x < canvas.width; x++){
 
-                //camera raay
-                Ray r(vec3(0,0,-10), vec3(u, -v,v_depth));
-                color += trace(r, scene);
-            }
-            int index = y * canvas.width + x;
-            pixels[index] = color/samples;
+              int index = y * canvas.width + x;
+              vec3 color = pixels[index];
+
+              int r = int (pow(clamp(color.x, 0, 1), 1/GAMMA) * 255);
+              int g = int (pow(clamp(color.y, 0, 1), 1/GAMMA) * 255);
+              int b = int (pow(clamp(color.z, 0, 1), 1/GAMMA) * 255);
+
+              canvas.setPixel(x,y,r,g, b);
+          }
+      }
+
+      auto finish = std::chrono::high_resolution_clock::now();
+      auto elapsed =  std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
+
+      std::cout << "Render Time: " << double(elapsed.count()) / 1000 << " seconds" << std::endl;
+
+      std::cout << "Saving image..." << std::endl;
+      canvas.savePPM("render/test-scene.ppm");
+    }
+ 
+    else{   
+      for (int y = my_rank - 1; y < canvas.height; y = y + comm_sz - 1){
+          //each thread needs seed for call to rand_r() because rand() is locking
+	  double color_array[canvas.width * 3];
+          unsigned int seed = y;
+          #pragma omp parallel for num_threads(num_threads)
+          for (int x = 0; x < canvas.width; x++){
+              vec3 color(0,0,0);
+
+              for (int i = 0; i < samples; i++){
+
+                  //compute location on projection plane
+                  double u = v_width * (double(x) + rand_real(&seed))/double(canvas.width) - (v_width/2);
+                  double v = v_height * (double(y) + rand_real(&seed))/double(canvas.height) - (v_height/2);
+
+                  //camera ray
+                  Ray r(vec3(0,0,-10), vec3(u, -v,v_depth));
+                  color += trace(r, scene);
+              }
+              //int index = y * canvas.width + x;
+              color = color/samples;
+	      color_array[x * 3] = color.x;
+	      color_array[x * 3 + 1] = color.y;
+	      color_array[x * 3 + 2] = color.z;
+          }
+	  MPI_Send(&color_array[0], canvas.width * 3, MPI_DOUBLE, 0, y, comm);
         }
     }
-    
-
-    //Tonemapping and vec to color conversion
-    for (int y = 0; y < canvas.height; y++){
-        for (int x = 0; x < canvas.width; x++){
-
-            int index = y * canvas.width + x;
-            vec3 color = pixels[index];
-
-            int r = int (pow(clamp(color.x, 0, 1), 1/GAMMA) * 255);
-            int g = int (pow(clamp(color.y, 0, 1), 1/GAMMA) * 255);
-            int b = int (pow(clamp(color.z, 0, 1), 1/GAMMA) * 255);
-
-            canvas.setPixel(x,y,r,g, b);
-        }
-    }
-
-    auto finish = std::chrono::high_resolution_clock::now();
-    auto elapsed =  std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
-
-    std::cout << "Render Time: " << double(elapsed.count()) / 1000 << " seconds" << std::endl;
-    
-    std::cout << "Saving image..." << std::endl;
-    canvas.savePPM("render/test-scene.ppm");
-   
+    MPI_Barrier(comm);
     return 0;
 }
 
@@ -124,7 +155,7 @@ vec3 trace(Ray r, Scene &scene){
     vec3 color;
 
     if (ray_hit) {
-         
+
         closest->computeHit(r, closest_dist, &hit, &normal);
 
         vec3 L = light - hit;
